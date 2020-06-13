e16a4972b7e8903ee0c0b0fee7fe1f117e8ca319
rahmimaulidiyah <rahmi1800016102@webmail.uad.ac.id>
2020-06-13 22:56:11 +0700

    first commit

---
+++ FormMhs.php
@@ -0,0 +1,43 @@
+<html>
+<body>
+
+
+
+<table border="1" align="center">
+<tr>
+     <td width="250">NIM</td>
+     <td width="20">:</td>
+     <td width="270"> <?php echo $_POST["NIM"] ?></td>
+</tr>
+<tr>
+     <td>NAMA</td>
+     <td>:</td>
+     <td><?php echo $_POST["NAMA"]?></td>
+</tr>
+<tr>
+     <td>JENIS KELAMIN</td>
+     <td>:</td>
+     <td><?php echo $_POST["KELAMIN"]?></td>
+</tr>
+<tr>
+     <td>ALAMAT</td>
+     <td>:</td>
+     <td><?php echo $_POST["ALAMAT"]?></td>
+</tr>
+<tr>
+     <td>PROGRAM STUDI</td>
+     <td>:</td>
+     <td><?php echo $_POST["PRODI"]?></td>
+</tr>
+<tr>
+     <td>BAHASA PEMROGRAMAN YANG DIKUASAI</td>
+     <td>:</td>
+     <td><?php echo $_POST["PEMROGRAMAN"]?></td>
+     
+</tr>
+<tr>
+     <td align="center" colspan="3"><a href="FormMhs.html">BACK </a></td>
+</tr>
+</table>
+</body>
+</html>
